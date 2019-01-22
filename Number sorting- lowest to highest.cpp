a5a0cc422022c7b2ba2dd5a15ed82849639e4886
MahStuff <georgs12@gmail.com>
2019-01-22 15:50:16 +0200

    Initial commit - testing git stuff

---
+++ Number sorting- lowest to highest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter 5 numbers for the array"<<endl;
+    int masivs [5];
+    for (int i=0; i<5; i++) {
+        cin>>masivs[i];
+    }
+    bool sorted=true;
+    int tmp, operation=0;
+    while (sorted==true) {
+        sorted=false;
+        for (int i=0; i<4; i++) {
+            if (masivs[i]>masivs[i+1]) {
+                operation++;
+                tmp=masivs[i];
+                masivs[i]=masivs[i+1];
+                masivs[i+1]=tmp;
+                sorted=true;
+
+            }
+            //else cout<<"Not entering"<<endl;
+        }
+    }
+
+    for (int i=0; i<5; i++){
+        cout<<masivs[i]<<endl;
+    }
+    cout<<"Operations performed: "<<operation<<endl;
+}
